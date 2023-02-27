cb416e9ec9eb7bc2da95857271333519df1e0d04
nuri0 <negaihoshinano@gmail.com>
2023-02-27 10:35:18 +0900

    1단계 max속도 변경

--- app/src/main/java/com/samin/carerobot/Logics/WheelSpeed.kt
+++ app/src/main/java/com/samin/carerobot/Logics/WheelSpeed.kt
@@ -3,7 +3,8 @@ package com.samin.carerobot.Logics
 import com.jeongmin.nurimotortester.Nuri.EnumCodesMap
 
 enum class WheelSpeed(val speed: Float) {
-    First(29f),
+    //원래 First 29f ->28f변경(속도 시험)
+    First(28f),
     Second(15f),
     Third(7.5f);
     companion object : EnumCodesMap<WheelSpeed, Float> by EnumCodesMap({ it.speed })
